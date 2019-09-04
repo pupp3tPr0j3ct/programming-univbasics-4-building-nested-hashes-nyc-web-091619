c61fb7735bd9fc0d74356d96ec2f874813fe6284
Jason Santana <jsantana006@gmail.com>
2019-09-04 17:32:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,8 +47,8 @@ def monopoly_with_fourth_tier
     :names => {
       :reading_railroad => {"mortgage_value" => "$100"},
       :pennsylvania_railroad => {"mortgage_value" => "$200"},
-      :b_and_o_railroad => {},
-      :shortline_railroad => {}
+      :b_and_o_railroad => {"mortgage_value" => "$400"},
+      :shortline_railroad => {"mortgage_value" => ""}
     },
     :pieces => 4,
     :rent_in_dollars => {
